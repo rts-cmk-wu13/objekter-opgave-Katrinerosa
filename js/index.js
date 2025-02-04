f629312be40cb5f8c63270872a793faaf4df7ff5
Katrinerosa <katrinerosa@gmail.com>
2025-02-04 10:34:51 +0100

    sofarsogood

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-// slå dig løs her... 
+// slå dig løs her...
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach(service => console.log(service.headline))
+services.forEach((service) => console.log(service.headline));
+let heroElm = document.querySelector(".hero");
+let heroDiv = document.createElement("div");
+heroDiv.innerHTML = `
+<img src="${hero.image}" alt="">
+<H1>${hero.headline}</H1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac amet id eget scelerisque amet etiam in sit commodo. Pretium ut duis orci pulvinar pretium id consectetur. </p>`;
+
+heroElm.append(heroDiv);
