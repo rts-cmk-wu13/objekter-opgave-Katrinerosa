f8acb5aeb1e502007b82eb540cf9bccbc4988ba8
Katrinerosa <katrinerosa@gmail.com>
2025-02-04 11:25:19 +0100

    sofar

--- js/index.js
+++ js/index.js
@@ -1,12 +1,34 @@
 // slå dig løs her...
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach((service) => console.log(service.headline));
+// services.forEach((service) => console.log(service.headline));
 let heroElm = document.querySelector(".hero");
 let heroDiv = document.createElement("div");
 heroDiv.innerHTML = `
 <img src="${hero.image}" alt="">
 <H1>${hero.headline}</H1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac amet id eget scelerisque amet etiam in sit commodo. Pretium ut duis orci pulvinar pretium id consectetur. </p>`;
+<p>${hero.copy}</p>
+<a href="#"><button class="explore">explore</button></a>`;
 
 heroElm.append(heroDiv);
+//----
+
+let servicesElm = document.querySelector(".services");
+services.forEach((service) => {
+  let article = document.createElement("article");
+  article.innerHTML = `
+    <img src="${service.illustration}" alt="">
+    <h3>${service.headline}</h3>
+    <p>${service.text}</p>
+    <a href="#"><p>${service.linktext}</p></a>
+
+    `;
+  servicesElm.append(article);
+});
+
+//===>
+
+    let facilitiesElm = document.querySelector(".facilities");
+    facilities.forEach((facility) => {
+        
+    })
