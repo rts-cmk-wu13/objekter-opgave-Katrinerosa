a586c4eff3573c4e74975886e3cbc66ace34916a
Katrinerosa <katrinerosa@gmail.com>
2025-02-05 10:01:09 +0100

    jeg ved ikke hvad jeg laver

--- js/index.js
+++ js/index.js
@@ -5,10 +5,14 @@
 let heroElm = document.querySelector(".hero");
 let heroDiv = document.createElement("div");
 heroDiv.innerHTML = `
+
 <img src="${hero.image}" alt="">
+<div class= "boks">
 <H1>${hero.headline}</H1>
 <p>${hero.copy}</p>
-<a href="#"><button class="explore">explore</button></a>`;
+<a href="#"><button class="explore">explore</button></a>
+</div>
+`;
 
 heroElm.append(heroDiv);
 //----
@@ -18,34 +22,65 @@ services.forEach((service) => {
   let article = document.createElement("article");
   article.innerHTML = `
     <img src="${service.illustration}" alt="">
+    <article class= "artikelkasse">
     <h3>${service.headline}</h3>
     <p>${service.text}</p>
-    <a href="#"><p>${service.linktext}</p></a>
+    <a href="#"><p>${service.linktext}</p></a></article>
 
     `;
   servicesElm.append(article);
 });
 
-//===>virker ikke mangler headline og options
+//===>virker
 
-    let facilitiesElm = document.querySelector(".facilities");
-    let facilitiesHeader = document.createElement("h2");
-    facilitiesHeader.textContent = facility.headline;
-    facilitiesElm.append(facilitiesHeader);
+let facilitiesElm = document.querySelector(".facilities");
+let facilitiesHeader = document.createElement("h2");
+facilitiesHeader.textContent = facilities.headline;
+facilitiesElm.append(facilitiesHeader);
 
-    let facilitiesList = document.createElement("div");
-    facilitiesList.classListadd("facilities-list");
-    facilities.forEach(facility => {
-        let facilityItem = document.createElement("div");
-        facilityItem.classList.add("facility-item");
+let facilitiesList = document.createElement("div");
+facilitiesList.classList.add("facilities-list");
+facilities.options.forEach((option) => {
+  let facilityItem = document.createElement("div");
+  facilityItem.classList.add("facility-item");
 
-
-    facilityItem.innerHTML = `
-    <img src="${facility.icon}" alt="${facility.title}">
-    <h3>${facility.title}</h3>
-    <p>${facility.description}</p>
+  facilityItem.innerHTML = `
+    <div class= "facility-kasser">
+        <img src="${option.icon}" alt="${option.title}">
+    <h3>${option.headline}</h3>
+    <p>${option.text}</p></div>
     `;
-    facilitiesList.append(facilityItem);
-    facilitiesElm.append(facilitiesList);
+  facilitiesList.append(facilityItem);
+  facilitiesElm.append(facilitiesList);
 });
 
+let sitesElm = document.querySelector(".sites");
+let sitesHeader = document.createElement("h2");
+sitesHeader.textContent = sites.headline;
+
+sitesElm.append(sitesHeader);
+
+sites.places.forEach((place) => {
+  let article = document.createElement("article");
+  article.innerHTML = `
+     <div class= "sites_kasser">
+    <article class= "kasse_site">
+        <img src="${place.img}" alt="">
+     <h3>${place.name}</h3>
+     <p>${place.city}</p>
+     <a href=""><button></button></a>
+    </article class= "kasse2">
+
+
+     <article>
+
+     </article>
+    
+    
+    </div> 
+
+ 
+        
+    `;
+  sitesElm.append(article);
+});
